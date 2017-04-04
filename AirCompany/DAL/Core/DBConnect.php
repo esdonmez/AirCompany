dcb74b0cc86acced1954155c0b2d9a43ffe5d671
Onur Celik <onrcelik@outlook.com>
2017-04-04 12:05:24 +0300

    edit DBConnect

--- AirCompany/DAL/Core/DBConnect.php
+++ AirCompany/DAL/Core/DBConnect.php
@@ -7,27 +7,27 @@ class DBConnect
     private $connection;
 
 
-   public function __construct() {
+    public function __construct() {
         $this.connection = new mysqli(SERVER, USER, PASS, DBNAME);
 
         if ($this->connection->connect_error) {
-				die("Error connection.: " . $this->connection->connect_error);
-		}
+	 			die("Error connection.: " . $this->connection->connect_error);
+	 	}
         
-		$this->connection->set_charset("utf8");
-   }
+	 	$this->connection->set_charset("utf8");
+    }
 
-   public function __destruct(){
-        $this->connection->close();
-   }
+    public function __destruct(){
+         $this->connection->close();
+    }
 
 
-   public function get($query){
-       $result = $this->connection->query($query);
-       return $result;
-   }
+    public function get($query){
+        $result = $this->connection->query($query);
+        return $result;
+    }
 
-   public function execute($query){
-       return ($this->connection->query($query) == TRUE);
-   }
+    public function execute($query){
+        return ($this->connection->query($query) == TRUE);
+    }
 }
