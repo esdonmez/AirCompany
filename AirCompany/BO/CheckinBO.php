2b0ca6393f9657875ca87a6c99e9e56d00e640a4
Elif Seray Dönmez <elifseraydonmez@gmail.com>
2017-04-11 21:19:36 +0300

    CheckinBO class created.

---
+++ AirCompany/BO/CheckinBO.php
@@ -0,0 +1,41 @@
+<?php
+
+class CheckinBO
+{
+    private $CheckId;
+    private $FlightId;
+    private $PNR;
+    private $Seat;
+    private $IsChecked;
+
+    public function CheckinBO($CheckId, $FlightId, $PNR, $Seat, $IsChecked){
+        $this->CheckId = $CheckId;
+        $this->FlightId = $FlightId;
+        $this->PNR = $PNR;
+        $this->Seat = $Seat;
+        $this->IsChecked = $IsChecked;
+    }
+
+
+    public function getCheckId(){
+		return $this->CheckId;
+	}
+
+    public function getFlightId(){
+        return $this->FlightId;
+    }
+
+    public function getPNR(){
+        return $this->PNR;
+    }
+
+    public function getSeat(){
+        return $this->Seat;
+    }
+
+    public function getIsChecked(){
+        return $this->IsChecked;
+    }
+}
+
+?>
