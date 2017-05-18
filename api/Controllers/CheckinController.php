697a4feb612980449dc8ac7ec1d084efdc26e7c5
Onur Celik <onrcelik@outlook.com>
2017-05-19 02:57:01 +0300

    log table and controller is editted.

--- api/Controllers/CheckinController.php
+++ api/Controllers/CheckinController.php
@@ -37,11 +37,11 @@ class CheckinController extends ApiController
             if($response == true){
                 $model->IsSuccess = true;
                 $model->Message = "check is added.";
-                LogHelper::Log("CheckinTable", "add a checkin");
+                LogHelper::Log("CheckinTable", $_SERVER['REMOTE_ADDR'], "add a checkin");
             } else{
                 $model->IsSuccess = false;
                 $model->Message = "check is not added.";
-                LogHelper::Log("CheckinTable", "cannot add the checkin");
+                LogHelper::Log("CheckinTable", $_SERVER['REMOTE_ADDR'], "cannot add the checkin");
             }
 
             $requestContentType = $_SERVER['HTTP_ACCEPT'];
@@ -51,7 +51,7 @@ class CheckinController extends ApiController
         }
 
         catch(Exception $e){
-            LogHelper::Log("CheckinTable", $e);
+            LogHelper::Log("CheckinTable", $_SERVER['REMOTE_ADDR'], $e);
         }
     }
 
@@ -79,12 +79,12 @@ class CheckinController extends ApiController
             $requestContentType = $_SERVER['HTTP_ACCEPT'];
             $this->setHttpHeaders($requestContentType, $statusCode);
 
-            LogHelper::Log("CheckinTable", "assign a seat");
+            LogHelper::Log("CheckinTable", $_SERVER['REMOTE_ADDR'], "assign a seat");
             echo json_encode($model);
         }
 
         catch(Exception $e){
-            LogHelper::Log("CheckinTable", $e);
+            LogHelper::Log("CheckinTable", $_SERVER['REMOTE_ADDR'], $e);
         }    
     }
 }
--- api/Helpers/LogHelper.php
+++ api/Helpers/LogHelper.php
@@ -5,12 +5,12 @@ require_once("Models/LoggingModel.php");
 
 class LogHelper
 {
-    public static function Log($entity, $operation){
+    public static function Log($entity, $ip, $operation){
         $dt = new DateTime();
         $createdate = $dt->format('Y-m-d H:i:s');
         
         $dbConnect = new DBConnect();
-        $response = $dbConnect->execute("INSERT INTO LoggingTable (Entity, Operation, CreateDate) VALUES ('$entity', '$operation', '$createdate')");
+        $response = $dbConnect->execute("INSERT INTO LoggingTable (Entity, Operation, IpAddress, CreateDate) VALUES ('$entity', '$ip', '$operation', '$createdate')");
 
         return $response;
     }
