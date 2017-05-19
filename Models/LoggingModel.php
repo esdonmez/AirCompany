589e9ee7b04f36b5f1069a0abb906614736a0c97
Onur Celik <onrcelik@outlook.com>
2017-05-19 03:29:37 +0300

    edit

--- Models/LoggingModel.php
+++ Models/LoggingModel.php
@@ -6,14 +6,18 @@ class LoggingModel
     private $Entity;
     private $Operation;
     private $CreateDate;
+    private $IpAddress;
+    private $IsSuccess;
 
 
-    public function __construct($Id, $Entity, $Operation, $CreateDate)
+    public function __construct($Id, $Entity, $Operation, $CreateDate, $IpAddress, $IsSuccess)
     {
         $this->Id = $Id;
         $this->Entity = $Entity;
         $this->Operation = $Operation;
         $this->CreateDate = $CreateDate;
+        $this->IpAddress = $IpAddress;
+        $this->IsSuccess = $IsSuccess;
     }
 
 
@@ -31,5 +35,13 @@ class LoggingModel
 
     public function getCreateDate(){
         return $this->CreateDate;
+    }
+
+    public function getIpAddress(){
+        return $this->IpAddress;
+    }
+
+    public function getIsSuccess(){
+        return $this->IsSuccess;
     }
 }
