acadd7704fffe422529b39273e3d2af672bf2928
Onur Celik <onrcelik@outlook.com>
2017-05-29 02:42:00 +0300

    try-catch

--- api/Controllers/FlightController.php
+++ api/Controllers/FlightController.php
@@ -21,30 +21,36 @@ class FlightController extends ApiController
 
     //[HttpPost]
     public function GetFlightDetails($FlightId){
-        $response = $this->dbConnect->get("SELECT F.Id, F.FlightNumber, F.Price, F.DepartureDateTime, F.ArrivalDateTime, F.Passanger,
+        try{
+            $response = $this->dbConnect->get("SELECT F.Id, F.FlightNumber, F.Price, F.DepartureDateTime, F.ArrivalDateTime, F.Passanger,
                                             A1.City as DepartureCity, A1.Code as DepartureAirportCode,
                                             A2.City as ArrivalCity, A2.Code as ArrivalAirportCode
                                             FROM FlightTable AS F LEFT JOIN AirportTable AS A1 ON F.DepartureId = A1.Id LEFT JOIN AirportTable AS A2 ON F.DestinationId = A2.Id
                                             WHERE F.Id='$FlightId'");
-        $data = $response->fetch_assoc();
+            $data = $response->fetch_assoc();
 
-        $model = new FlightDetailModel();
-        $model->Id = $data["Id"];
-        $model->FlightNumber = $data["FlightNumber"];
-        $model->DepartureDateTime = $data["DepartureDateTime"];     
-        $model->ArrivalDateTime = $data["ArrivalDateTime"];
-        $model->Passanger = $data["Passanger"];
-        $model->Price = $data["Price"];
-        $model->DepartureCity = $data["DepartureCity"];
-        $model->DepartureAirportCode = $data["DepartureAirportCode"];
-        $model->ArrivalCity = $data["ArrivalCity"];
-        $model->ArrivalAirportCode = $data["ArrivalAirportCode"];
+            $model = new FlightDetailModel();
+            $model->Id = $data["Id"];
+            $model->FlightNumber = $data["FlightNumber"];
+            $model->DepartureDateTime = $data["DepartureDateTime"];     
+            $model->ArrivalDateTime = $data["ArrivalDateTime"];
+            $model->Passanger = $data["Passanger"];
+            $model->Price = $data["Price"];
+            $model->DepartureCity = $data["DepartureCity"];
+            $model->DepartureAirportCode = $data["DepartureAirportCode"];
+            $model->ArrivalCity = $data["ArrivalCity"];
+            $model->ArrivalAirportCode = $data["ArrivalAirportCode"];
 
-        $requestContentType = $_SERVER['HTTP_ACCEPT'];
-        $this->setHttpHeaders($requestContentType, $statusCode);
-            
-        LogHelper::Log("FlightTable", "show flight details", "true");
-        echo json_encode($model);
+            $requestContentType = $_SERVER['HTTP_ACCEPT'];
+            $this->setHttpHeaders($requestContentType, $statusCode);
+                
+            LogHelper::Log("FlightTable", "show flight details", "true");
+            echo json_encode($model);
+        }
+        
+        catch(Exception $e){
+            LogHelper::Log("FlightTable", $e, "false");
+        }
     }
 
     //[HttpPost]
